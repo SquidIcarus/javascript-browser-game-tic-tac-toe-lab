d0500e650a1e6174532369988ec703917ff77113
User Name <landonisaiah@gmail.com>
2025-05-19 02:22:38 -0700

    step 1 and 2 complete

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 
 
 
-/*---------------------------- Variables (state) ----------------------------*/
+let board = null;
+let turn = null;
+let winner = null;
+let tie = null;
 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.getElementById("message");
 
+console.log(squareEls);
+console.log(messageEl);
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -15,6 +22,4 @@
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
 
