a0517a5b16444a209cc9d57cce5f8ddd00d10064
User Name <landonisaiah@gmail.com>
2025-07-14 00:09:09 -0700

    step 4 complete with values input manually to see turns rendered on board

--- js/app.js
+++ js/app.js
@@ -12,19 +12,48 @@ let tie = null;
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById("message");
 
-console.log(squareEls);
-console.log(messageEl);
+// console.log(squareEls);
+// console.log(messageEl);
 
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {    // function to initialize the game upon loading
-    console.log('board initializing')
-    board = ['', '', '', '', '', '', '', '', '']; // sets the board to nine empty strings
+    console.log('board initialized')
+    board = ['X', 'O', '', '', '', '', '', '', '']; // sets the board to nine empty strings
     turn = 'X'; // turn set to start with 'X'
     winner = false; // winner set to false, no winner yet
     tie = false; // tie set to false
     render(); // calling the render function 
 };
 
-/*----------------------------- Event Listeners -----------------------------*/
+function updateBoard() {
+    board.forEach((cell, index) => {      // forEach to loop through board array
+        const squareEl = squareEls[index]; // using the index t oaccess the corresponding squareEls
+        if (cell === 'X') {                // update the square depending on the value of each cell
+            squareEl.textContent = 'X';
+        } else if (cell === 'O') {
+            squareEl.textContent = 'O';
+        } else {
+            squareEl.textContent = '';
+        }
+    });
+};
 
+function updateMessage() {
+    if (winner === false && tie === false) {   // if no winner and not a tie
+        messageEl.textContent = `It's ${turn}'s turn`;  // then render whos turn it is based on the `turn` variable
+    } else if (winner === false && tie === true) {   // if there is no winner and it's a tie
+        messageEl.textContent = `Cat's Game! 😸`;    // then render a 'tie' message
+    } else {
+        messageEl.textContent = `Congratulations ${turn} Wins!`;  // Otherwise, congratulations player based on `turn` variable has won!
+    }
+};
+
+
+function render() {
+    updateBoard();
+    updateMessage();
+};
+
+/*----------------------------- Event Listeners -----------------------------*/
+init();
