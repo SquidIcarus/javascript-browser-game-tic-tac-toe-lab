c5a352262f43a8d8186de6165777db80e129e30e
User Name <landonisaiah@gmail.com>
2025-07-13 23:07:10 -0700

    add init function to initialize game state on load

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
-let board = null;
+let board = ['', '', '', '', '', '', '', '', '']; // changed to array that represents all nine boxes
 let turn = null;
 let winner = null;
 let tie = null;
 
-
-
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll(".sqr");
+const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById("message");
 
 console.log(squareEls);
 console.log(messageEl);
-
-
 /*-------------------------------- Functions --------------------------------*/
 
-
+function init() {    // function to initialize the game upon loading
+    console.log('board initializing')
+    board = ['', '', '', '', '', '', '', '', '']; // sets the board to nine empty strings
+    turn = 'X'; // turn set to start with 'X'
+    winner = false; // winner set to false, no winner yet
+    tie = false; // tie set to false
+    render(); // calling the render function 
+};
 
 /*----------------------------- Event Listeners -----------------------------*/
 
